4de2345f954257b0042bc8197bbea073b467a6d9
Ritesh Arya <sarya0113@gmail.com>
2022-06-21 09:10:26 +0000

    Added s3 bucket module 2

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,5 @@
-# This S3 bucket will not be created
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  create_bucket = false
-  # ... omitted
+module "s3-bucket" {
+  source  = "app.terraform.io/ritesharya-training/s3-bucket/aws"
+  version = "2.8.0"
+  bucket_prefix = "ritarya"
 }
